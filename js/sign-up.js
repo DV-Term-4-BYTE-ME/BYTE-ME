ba8510a3e8fc48b5311601a989fd1801f88a3cf0
GabyNor05 <160027369+GabyNor05@users.noreply.github.com>
2024-10-07 15:00:38 +0200

    fixed js filing, organised buttons, attempted to write to json

---
+++ js/sign-up.js
@@ -0,0 +1,20 @@
+// import { writeFileSync } from "fs";
+
+// // converting a JS object to JSON
+// const user = {
+//   id: 1,
+//   completeName: "Jennifer Jones",
+//   age: 20,
+// };
+// const data = JSON.stringify(user);
+
+// try {
+//   // reading a JSON file synchronously
+//   writeFileSync("data.json", data);
+// } catch (error) {
+//   // logging the error
+//   console.error(error);
+//   throw error;}
+
+// // logging the outcome
+// console.log("data.json written correctly");
