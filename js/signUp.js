6e0abdd196df15c9890ad80a459f61f50d6dfa91
GabyNor05 <160027369+GabyNor05@users.noreply.github.com>
2024-10-30 15:59:23 +0200

    waiting for node.js

--- js/signUp.js
+++ js/signUp.js
@@ -1,6 +1,6 @@
-/*  import { writeFileSync } from "fs";
+/* import { writeFileSync } from "fs";
 
-// // converting a JS object to JSON
+//converting a JS object to JSON
  const user = {
    id: 1,
    completeName: "Jennifer Jones",
@@ -17,9 +17,7 @@
    throw error;}
 
 //logging the outcome
- console.log("data.json written correctly"); */
-
-//  enabling the Popover
+ console.log("data.json written correctly");  */
 
 
 document.getElementById("signUpButton").addEventListener("submit", onSubmit);
@@ -31,15 +29,56 @@ class User{
     this.password = password;
   }
 
+  getName(){
+    return this.name;
+  }
+
+  getEmail(){
+    return this.email;
+  }
+
+  getPassword(){
+    return this.password;
+  }
  } 
 
 
  function onSubmit(){
   //Check if it is valid
     let user1 = User(document.getElementsByClassName("sign-up-Name-InputBox").innerHTML,document.getElementById('sign-up-Email-InputBox').innerHTML,document.getElementsById("sign-up-Password-InputBox").innerHTML);
-
-    console.log(user1);
-    
+    console.log(user1);   
  }
+
+const fs = require('fs');
+import { readFileSync, writeFileSync } from 'fs';
+
+// Read the contents of the JSON file
+const data = readFileSync('userDetails.json');
+// Parse the JSON data into a JavaScript object
+const jsonData = JSON.parse(data);
+
+console.log("Before Adding data",JSON.stringify(jsonData, null, 4));
+
+// Modify the JavaScript object by adding new data
+jsonData.users.push({
+    name: user1.getName(),
+    email: user1.getEmail(),
+    password: user1.getPassword()
+});
+
+
+// Convert the JavaScript object back into a JSON string
+const jsonString = JSON.stringify(jsonData);
+
+writeFileSync('userDetails.json', jsonString, 'utf-8', (err) => {
+  if (err) throw err;
+  console.log('Data added to file');
+});
+
+const update_data = readFileSync('userDetails.json');
+const updated_jsonData = JSON.parse(update_data);
+console.log("After Adding data",JSON.stringify(updated_jsonData, null, 4));
+
+
 
  
