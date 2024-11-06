25885468c871dc3b484bddebdbe4c63a85a3c805
GabyNor05 <160027369+GabyNor05@users.noreply.github.com>
2024-11-06 14:29:52 +0200

    ss

--- js/signUp.js
+++ js/signUp.js
@@ -1,16 +1,18 @@
 let loginForm = document.getElementById("sign-up-form");
-loginForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let password = document.getElementById("sign-up-Password-InputBox").value;
-  let confirmPassword = document.getElementById(
-    "sign-up-ConfirmPassword-InputBox"
-  ).value;
-  let email = document.getElementById("sign-up-Email-InputBox").value;
-  
-  if (password != confirmPassword) {
-    alert("Your passwords do not match, try again.");
-  } else {
-    /* if (email === localStorage.getItem(User.email)) {
+loginForm.addEventListener(
+  "submit",
+  (e) => {
+    e.preventDefault();
+    let password = document.getElementById("sign-up-Password-InputBox").value;
+    let confirmPassword = document.getElementById(
+      "sign-up-ConfirmPassword-InputBox"
+    ).value;
+    let email = document.getElementById("sign-up-Email-InputBox").value;
+
+    if (password != confirmPassword) {
+      alert("Your passwords do not match, try again.");
+    } else {
+      /* if (email === localStorage.getItem(User.email)) {
       function myFunction() {
         myFunction();
         if (
@@ -19,25 +21,26 @@ loginForm.addEventListener("submit", (e) => {
           window.location.href = "../pages/signIn.html";
         }
       } */
-    /* } else { */
-      
-        $("#overlay").fadeIn(300);
-        let user = new User(
-          document.getElementById("sign-up-Name-InputBox").value,
-          document.getElementById("sign-up-Email-InputBox").value,
-          document.getElementById("sign-up-Password-InputBox").value
-        );
+      /* } else { */
 
-        const jsonString = JSON.stringify(user);
-        let userEmail = localStorage.setItem(user.email, jsonString);
-        let userPassword = localStorage.setItem(user.password, jsonString);
-        setTimeout(function () {
-          window.location.href = "../pages/signIn.html";
-        }, 5000);
-      }
+      $("#overlay").fadeIn(300);
+      let user = new User(
+        document.getElementById("sign-up-Name-InputBox").value,
+        document.getElementById("sign-up-Email-InputBox").value,
+        document.getElementById("sign-up-Password-InputBox").value
+      );
+
+      const jsonString = JSON.stringify(user);
+      let userEmail = localStorage.setItem(user.email, jsonString);
+      let userPassword = localStorage.setItem(user.password, jsonString);
+      setTimeout(function () {
+        window.location.href = "../pages/signIn.html";
+      }, 5000);
     }
- /*  } */
-/* } */);
+  }
+  /*  } */
+  /* } */
+);
 
 class User {
   constructor(name, email, password) {
